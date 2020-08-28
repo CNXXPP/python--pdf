b507ecbbf7fe37ee9a5f9517bc569a2898ee6453
xupan <xupan19941123@gmail.com>
2020-08-28 09:47:02 +0800

    feat: NMF and PAC decomposition imag

--- cluster/NMF.py
+++ cluster/NMF.py
@@ -16,4 +16,22 @@ def plot_gallery(title, images, n_col=n_col, n_row=n_row):
 
     for i, comp in enumerate(images):
         plt.subplot(n_row, n_col, i+1)
-        vmax = max()
+        vmax = max(comp.max(),-comp.min())
+
+        plt.imshow(comp.reshape(image_shape),cmap=plt.cm.gray,interpolation='nearest',vmin=-vmax,vmax=vmax)
+        plt.xticks(())
+        plt.yticks(())
+    plt.subplots_adjust(0.01,0.05,0.99,0.93,0.04,0.)
+
+estimators = [
+    ('Eigenfaces - PCA using randomized SVD',
+     decomposition.PCA(n_components=6,whiten=True)),
+    ('Non-negative components - NMF',
+     decomposition.NMF(n_components=6,init='nndsvda',
+                       tol=5e-3))]
+
+for name,estimator in estimators:
+    estimator.fit(faces)
+    components_=estimator.components_
+    plot_gallery(name,components_[:n_components])
+plt.show()
